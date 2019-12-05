ea6f89b2d2fa01d672b1d921890554f83f85708d
foldedtoad <foldedtoad@gmail.com>
2019-12-05 10:35:50 -0800

    add accelerometer example.

---
+++ apps/accel/accel.h
@@ -0,0 +1,17 @@
+/*
+ *  accel.h
+ *
+ * Copyright (c) 2018, Yannis Damigos
+ * Copyright (c) 2019, Callender-Consulting LLC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __ACCEL_H__
+#define __ACCEL_H__
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+void accel_init(void);
+
+#endif  // 
